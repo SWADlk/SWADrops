6af524062dafbb2e4ad1fbf7358e60a7597c3f01
SWADlk <snsshoplk@gmail.com>
2025-04-20 09:30:48 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ fetch('https://script.google.com/macros/s/AKfycbxB1eK89gWBtP2OaGjkabUiDih8bR5xN5
       card.innerHTML = `
         <img src="${drop.image}" alt="${drop.name}" />
         <h3>${drop.name}</h3>
-        <p>${drop.description}</p>
         <a href="airdrop.html?id=${drop.id}" class="detail-link">View Details</a>
       `;
       container.appendChild(card);
